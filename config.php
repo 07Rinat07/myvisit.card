b7bbcacbf7e02b7b0361c864c049f8a4b727f8ba
Rinat Sarmuldin <ura07srr@gmail.com>
2024-05-25 14:59:12 +0500

    Fix-update config file(https-http for settings site)

--- config.php
+++ config.php
@@ -1,8 +1,15 @@
 <?php
 
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $protocol = "https://";
+} else {
+    $protocol = "http://";
+}
+
+
 //host site для ссылок в браузере
-define('HOST', '//' . $_SERVER['HTTP_HOST'] .'/');
+define('HOST', $protocol . $_SERVER['HTTP_HOST'] .'/');
 
 // физич путь к корневой директории скрипта
-define('ROOT', dirname(__FILE__). '/');
+define('ROOT', dirname(__FILE__) . '/');
 
