f044e06fc1fda7e523f6c8aa0a44d071ca42d0c5
Rinat Sarmuldin <ura07srr@gmail.com>
2024-05-25 18:11:11 +0500

    Route

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 require_once "config.php";
 
-echo "<h1>index.php</h1>";
-echo HOST;
-echo "<br>";
-echo ROOT;
+// ROUTE
+$uri = $_SERVER["REQUEST_URI"];
+$uri = rtrim($uri, "/");
+$uri = filter_var($uri, FILTER_SANITIZE_URL);
+$uri = substr($uri, 1);
+$uri = explode('?', $uri);
+
+print_r($uri);
+
+switch ($uri[0]){
+    case '':
+        echo "Main page";
+        break;
+    case 'about':
+        echo "About me";
+        break;
+    case 'blog':
+        echo "Blog";
+        break;
+    case 'contacts':
+        echo "Contact page";
+        break;
+    default:
+        echo "Main or 404";
+        break;
+}
