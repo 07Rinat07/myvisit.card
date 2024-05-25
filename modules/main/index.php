8767214dd61e7b456bfed27e8329f53f85d90e9a
Rinat Sarmuldin <ura07srr@gmail.com>
2024-05-25 22:31:49 +0500

    Template engine(Output Buffering)

--- modules/main/index.php
+++ modules/main/index.php
@@ -1,5 +1,17 @@
 <?php
 
-include ROOT . 'templates/_parts/_header.tpl';
+//$content = "Main page";
+
+$page_name = "Главная страница";
+$page_text = "Текст главной страницы. Lorem ipsum.........";
+
+//Буферизация вывода
+ob_start();
 include ROOT . 'templates/main/main.tpl';
+$content = ob_get_contents();
+ob_end_clean();
+
+include ROOT . 'templates/_parts/_header.tpl';
+include ROOT . 'templates/template.tpl';
 include ROOT . 'templates/_parts/_footer.tpl';
+
--- templates/about/about.tpl
+++ templates/about/about.tpl
@@ -1,1 +1,8 @@
+<h1><?php echo $page_name; ?></h1>
+<?php echo $page_text; ?>
+
+
+<hr>
+
 <h2>About.tpl</h2>
+
