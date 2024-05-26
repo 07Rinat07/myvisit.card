5bb5ca7944111cb007950a03ff78db1c9b88394f
Rinat Sarmuldin <ura07srr@gmail.com>
2024-05-26 17:22:55 +0500

    Template for Login and Registration pages

--- modules/login/registration.php
+++ modules/login/registration.php
@@ -1,6 +1,15 @@
 <?php
 
-echo "<h1>registration.php</h1>";
+$pageTitle = "Регистрация";
+
+ob_start();
+include ROOT . 'templates/login/form-registration.tpl';
+$content = ob_get_contents();
+ob_end_clean();
+
+include ROOT . 'templates/_page-parts/_head.tpl';
+include ROOT . 'templates/login/login-page.tpl';
+include ROOT . 'templates/_page-parts/_foot.tpl';
 
 
 
