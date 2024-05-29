0a9a1d8a2390f965fbbc5166f2c8cded10ab2c07
Rinat Sarmuldin <ura07srr@gmail.com>
2024-05-29 18:09:39 +0500

    Create-ADD Password recovery template

--- modules/login/lost-password.php
+++ modules/login/lost-password.php
@@ -1,6 +1,15 @@
 <?php
 
-echo "<h1>lost-password.php</h1>";
+$pageTitle = "Восстановить пароль";
 
 
 
+
+ob_start();
+include ROOT . 'templates/login/lost-password.tpl';
+$content = ob_get_contents();
+ob_end_clean();
+
+include ROOT . 'templates/_page-parts/_head.tpl';
+include ROOT . 'templates/login/login-page.tpl';
+include ROOT . 'templates/_page-parts/_foot.tpl';
