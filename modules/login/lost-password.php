61118749783f0aa44eda9522a4443519cc1b885f
Rinat Sarmuldin <ura07srr@gmail.com>
2024-05-30 14:59:58 +0500

    Generating a secret code

--- modules/login/lost-password.php
+++ modules/login/lost-password.php
@@ -2,12 +2,52 @@
 
 $pageTitle = "Восстановить пароль";
 
-// TODO 1. Проверить отправку формы POST.
-// TODO 2. Проверка на заполненный email // info@mail.com.
-// TODO 3. Проверка существующего email.
-// TODO 4. Cгенерировать секретный код для сброса пароля.
-// TODO 5. Запомнить секретный код. Записать его в базу данных.
-// TODO 6. Присылаем пользователю спец ссылку с секретным кодом для установки нового пароля.
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+// 1. Проверить отправку формы POST
+if (isset($_POST['lost-password'])) {
+
+    $_POST['email'] = trim($_POST['email']);
+
+    // 2. Проверка на заполненный email // info@mail.com
+    if (trim($_POST['email']) == '') {
+        $errors[] = ['title' => 'Введите Email', 'desc' => '<p>Email обязателен для регистрации на сайте</p>'];
+    } else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors[] = ['title' => 'Введите корректный Email'];
+    }
+
+    if (empty($errors)) {
+
+        // 3. Проверка существующего email
+        $user = R::findOne('users', 'email = ?', array($_POST['email']));
+
+        if ($user) {
+            // 4. Сгенерировать секретный код для сброса пароля
+
+            // Генерируем секретный код
+            function random_str($num = 30)
+            {
+                return substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, $num);
+            }
+            $recovery_code = random_str();
+
+            // 5. Запомнить секретный код. Записать его в БД.
+            $user->recovery_code = $recovery_code;
+            R::store($user);
+        } else {
+            $errors[] = ['title' => 'Неверный Email'];
+        }
+    }
+}
+
+
+
+
+
+
+// 6. Присылаем пользователю специальную ссылку с секретным кодом для установки нового пароля
 
 
 ob_start();
@@ -18,4 +58,3 @@ ob_end_clean();
 include ROOT . 'templates/_page-parts/_head.tpl';
 include ROOT . 'templates/login/login-page.tpl';
 include ROOT . 'templates/_page-parts/_foot.tpl';
-
