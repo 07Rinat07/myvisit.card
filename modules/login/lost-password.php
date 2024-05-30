e78b5bc28fcfed9cdbb53986b405650ff3dc557e
Rinat Sarmuldin <ura07srr@gmail.com>
2024-05-30 13:56:48 +0500

    Planning a module for Password Recovery

--- modules/login/lost-password.php
+++ modules/login/lost-password.php
@@ -2,7 +2,12 @@
 
 $pageTitle = "Восстановить пароль";
 
-
+// TODO 1. Проверить отправку формы POST.
+// TODO 2. Проверка на заполненный email // info@mail.com.
+// TODO 3. Проверка существующего email.
+// TODO 4. Cгенерировать секретный код для сброса пароля.
+// TODO 5. Запомнить секретный код. Записать его в базу данных.
+// TODO 6. Присылаем пользователю спец ссылку с секретным кодом для установки нового пароля.
 
 
 ob_start();
@@ -13,3 +18,4 @@ ob_end_clean();
 include ROOT . 'templates/_page-parts/_head.tpl';
 include ROOT . 'templates/login/login-page.tpl';
 include ROOT . 'templates/_page-parts/_foot.tpl';
+
