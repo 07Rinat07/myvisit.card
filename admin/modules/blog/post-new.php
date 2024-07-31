66c9efdbd5251d1ddac4c22c468ca3877e59e544
Rinat Sarmuldin <ura07srr@gmail.com>
2024-07-31 18:01:31 +0500

    Preparation for templates in the admin panel

--- admin/modules/blog/post-new.php
+++ admin/modules/blog/post-new.php
@@ -1,1 +1,14 @@
 <h1>Admin - Blog - New Post</h1>
+
+<?php
+
+
+
+// central part
+ob_start();
+include ROOT . 'admin/templates/blog/post-new.tpl';
+$content = ob_get_contents();
+ob_end_clean();
+
+
+include ROOT . 'admin/templates/template.tpl';
