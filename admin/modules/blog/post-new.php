e946cc2ede5092630dc214ceaaa21daa701a6605
Rinat Sarmuldin <ura07srr@gmail.com>
2024-07-31 18:10:58 +0500

    Template for the Add Post page

--- admin/modules/blog/post-new.php
+++ admin/modules/blog/post-new.php
@@ -1,14 +1,12 @@
-<h1>Admin - Blog - New Post</h1>
-
 <?php
 
 
 
-// central part
+// Центральный шаблон для модуля
 ob_start();
 include ROOT . 'admin/templates/blog/post-new.tpl';
 $content = ob_get_contents();
 ob_end_clean();
 
-
+// Шаблон страницы
 include ROOT . 'admin/templates/template.tpl';
