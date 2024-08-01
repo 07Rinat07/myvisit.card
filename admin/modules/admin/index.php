94e4643e2121059de3252e9ec3bd0bf673bc6cb8
Rinat Sarmuldin <ura07srr@gmail.com>
2024-08-01 20:49:12 +0500

    Main page in admin panel

--- admin/modules/admin/index.php
+++ admin/modules/admin/index.php
@@ -1,1 +1,11 @@
-<h1>Admin - index.php</h1>
+<?php
+
+
+// Центральный шаблон для модуля
+ob_start();
+include ROOT . 'admin/templates/main/main.tpl';
+$content = ob_get_contents();
+ob_end_clean();
+
+// Шаблон страницы
+include ROOT . 'admin/templates/template.tpl';
