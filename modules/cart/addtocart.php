b5c4696be1e00ba432a38970fb8eda6655be57ad
Rinat Sarmuldin <ura07srr@gmail.com>
2024-08-27 13:15:16 +0500

    Adding one product to cart JSON format

--- modules/cart/addtocart.php
+++ modules/cart/addtocart.php
@@ -1,7 +1,16 @@
 <?php
 
+// Получаем пользователя из БД
 $user = R::load('users', $_SESSION['logged_user']['id']);
-$user->cart = $_GET['id'];
+
+// Формируем корзину в виде массива
+$cart = [$_GET['id'] => 1];
+
+// Превращаем корзину в JSON строку,
+// и записываем ее в поле cart для текущего пользователя
+$user->cart = json_encode($cart);
+
+// Обновляем пользователя в БД
 R::store($user);
 
 header("Location: " . HOST . "shop/" . $_GET['id']);
