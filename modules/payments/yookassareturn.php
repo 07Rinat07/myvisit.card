d2b11d581a86b8affcf0a5ac317626a2457d529e
Rinat Sarmuldin <ura07srr@gmail.com>
2024-09-25 23:21:23 +0500

    Update order information

--- modules/payments/yookassareturn.php
+++ modules/payments/yookassareturn.php
@@ -22,6 +22,32 @@ $paymentDB = R::load('payments', $_SESSION['payment']['id']);
 $paymentDB->status = $payment['status'];
 R::store($paymentDB);
 
+// Обновить информацию в заказе
+$order = R::load('orders', $paymentDB['order_id']);
+
+switch($payment['status']) {
+    case 'pending':
+        $order->status = 'pending';
+        $order->paid = false;
+        break;
+    case 'waiting_for_capture':
+        $order->status = 'waiting';
+        $order->paid = false;
+        break;
+    case 'succeeded':
+        $order->status = 'paid';
+        $order->paid = true;
+        break;
+    case 'canceled':
+        $order->status = 'canceled';
+        $order->paid = false;
+        break;
+    default:
+        break;
+}
+
+R::store($order);
+
 // Шаблоны
 include ROOT . 'templates/_page-parts/_head.tpl';
 include ROOT . 'templates/_parts/_header.tpl';
@@ -34,5 +60,6 @@ include ROOT . 'templates/_page-parts/_foot.tpl';
 + Страница возврата, шаблон
 + Получить статус платежа из Юкассы
 + Запись статуса оплаты в БД
++ Обновить информацию в заказе: оплачен/не оплачен
 - Шаблоны для статуса оплаты на странице возврата
 */
