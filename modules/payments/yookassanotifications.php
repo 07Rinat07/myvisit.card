9e13e929edade817880410e0579c6284edf10d1b
Rinat Sarmuldin <ura07srr@gmail.com>
2024-09-26 16:22:52 +0500

    Notifications. Settings in Yookassa

--- modules/payments/yookassanotifications.php
+++ modules/payments/yookassanotifications.php
@@ -1,3 +1,9 @@
 <?php
 
 echo 'yookassanotifications.php';
+
+// Записываем полученные данные в файл. Для отладки
+$file = "yookassanotify.txt";
+$res = date('[Y-m-d H:i:s] ') . PHP_EOL;
+$res .=  "\r\n \r\n";
+file_put_contents($file, $res, FILE_APPEND | LOCK_EX);
