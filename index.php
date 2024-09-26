070679d39969bea19fec14e9d8b4bf3ed64b89c3
Rinat Sarmuldin <ura07srr@gmail.com>
2024-09-26 16:13:26 +0500

    Fix index php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+error_reporting(E_ALL & ~E_DEPRECATED); // отключил показ ошибок несовместимости библиотеки с версиями PHP 8 и новее.
+
+
+
+
 require __DIR__ . '/vendor/autoload.php';
 
 require_once "config.php";
